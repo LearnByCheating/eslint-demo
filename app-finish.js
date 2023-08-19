f6a54369181993fb3c28346817f5bcbb7d97510e
Steve C <steve@example.com>
2023-08-19 11:18:00 -0700

    v2-ESLint-ending-code

---
+++ app-finish.js
@@ -0,0 +1,10 @@
+/*
+  Click squiggly lines to see the rule that is violated, and the options to fix or disable the rule for the line or the entire file.
+*/
+
+const double = (num) => {
+  const res = num * 2;
+  return res;
+};
+const res = double(10);
+console.log(`Function result: ${res}`);
